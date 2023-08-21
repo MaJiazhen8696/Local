928f0436d8785374a2d6be6a85c53bd83357054a
FakerMa <40601277+FakerMa@users.noreply.github.com>
2023-08-21 13:50:03 +0800

    Add files via upload

---
+++ common.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifdef LOCAL
+    freopen("in.in","r",stdin);
+    freopen("out.out","w",stdout);
+#endif 
+    cout<<1<<endl;
+
+    return 0;
+}
